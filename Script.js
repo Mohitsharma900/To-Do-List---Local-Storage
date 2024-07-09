abf32ec007b841bd568380119eea6110b592d871
Mohit <168701475+Mohitsharma900@users.noreply.github.com>
2024-07-09 20:56:42 +0530

    Completed without local storage.

--- Script.js
+++ Script.js
@@ -14,20 +14,48 @@ let delete_li = document.createElement('img');
 add.addEventListener('click', () => {
     let value = task_Value.value;
     let li = document.createElement('li');
-
+    let task_text = document.createElement('span');
+    task_text.textContent = value;
+    li.appendChild(task_text)
+    // let li_tick = document.createElement('li')
+    // let li_del = document.createElement('li')
     if (value.trim() === "") return;
-    
+
+    let tick = document.createElement('img');
+    tick.src = "Assets/Images/check-solid.svg"
+    tick.classList.add('tick_icon')
+
+    tick.addEventListener('click', (event)=>{
+        let parent=  event.target.parentElement;
+        parent.parentElement.remove();
+        let task_completed_ul = document.querySelector('.task-completed-ul');
+        let Li = document.createElement('li');
+        Li = parent.parentElement;
+        ul_li.removeChild(tick)
+        task_completed_ul.appendChild(Li)
+    })
+
     let delete_li = document.createElement('img');
     delete_li.src = "Assets/Images/trash-solid.svg";
     delete_li.classList.add('delete-icon');
 
     delete_li.addEventListener('click', (event)=>{
         let Parent = event.target.parentElement
-        Parent.remove()
+        Parent.parentElement.remove()
     })
 
-    li.innerHTML = value;
-    li.appendChild(delete_li);
+    
+
+    let ul_li= document.createElement('ul');
+    ul_li.appendChild(tick);
+    ul_li.appendChild(delete_li);
+    li.appendChild(ul_li)
+    ul_li.classList.add('ul-li')
+
+
+    // li.innerText = value;
+    // li.appendChild(tick)
+    // li.appendChild(delete_li);
     ul.appendChild(li);
     
     console.log(value);
