cca43fe1ee251bcc6ff2ceded290a0e039697f28
Mohit <168701475+Mohitsharma900@users.noreply.github.com>
2024-07-16 11:10:55 +0530

    commit

---
+++ script_1.js
@@ -0,0 +1,131 @@
+/**
+ * Initialize
+ */
+
+let task_Value = document.querySelector('.input-text');
+let add = document.querySelector(".material-icons")
+let ul = document.querySelector(".tasks_pending ul")
+let delete_li = document.createElement('img');
+let task_idx = 0
+let local_storage = localStorage
+
+/**
+ * functionalities
+ */
+
+window.addEventListener('load', ()=>{
+    for (let i = 1; i < localStorage.length; i++) {
+        console.log(localStorage);
+
+        let li = document.createElement('li');
+        let task_text = document.createElement('span');
+
+        task_text.textContent = `${localStorage.key(iz)}`;
+        ul.appendChild(li);
+        li.appendChild(task_text);
+
+        let tick = document.createElement('img');
+        tick.src = "Assets/Images/check-solid.svg"
+        tick.classList.add('tick_icon')
+
+    tick.addEventListener('click', (event)=>{
+        let parent=  event.target.parentElement;
+        parent.parentElement.remove();
+        let task_completed_ul = document.querySelector('.task-completed-ul');
+        let Li = document.createElement('li');
+        Li = parent.parentElement;
+        ul_li.removeChild(tick)
+        task_completed_ul.appendChild(Li)
+    })
+
+    let delete_li = document.createElement('img');
+    delete_li.src = "Assets/Images/trash-solid.svg";
+    delete_li.classList.add('delete-icon');
+
+    delete_li.addEventListener('click', (event)=>{
+        localStorage.removeItem(`${li.id}`)
+        let Parent = event.target.parentElement
+        Parent.parentElement.remove()
+    })
+
+    let ul_li= document.createElement('ul');
+    ul_li.appendChild(tick);
+    ul_li.appendChild(delete_li);
+    li.appendChild(ul_li)
+    ul_li.classList.add('ul-li')
+        
+
+    }
+})
+
+add.addEventListener('click', () => {
+    task_idx++
+    let value = task_Value.value;
+    let li = document.createElement('li');
+    localStorage.setItem(`${task_idx}` , `${task_Value.value}`);
+    let local_value =  localStorage.getItem(`${task_idx}` , `${task_Value.value}`)
+    console.log(local_value);
+    li.setAttribute('id', `${task_idx}`)
+    console.log(li.id);
+
+    let task_text = document.createElement('span');
+    task_text.textContent = local_value;
+    li.appendChild(task_text)
+    // let li_tick = document.createElement('li')
+    // let li_del = document.createElement('li')
+    if (value.trim() === "") return;
+
+    let tick = document.createElement('img');
+    tick.src = "Assets/Images/check-solid.svg"
+    tick.classList.add('tick_icon')
+
+    tick.addEventListener('click', (event)=>{
+        let parent=  event.target.parentElement;
+        parent.parentElement.remove();
+        let task_completed_ul = document.querySelector('.task-completed-ul');
+        let Li = document.createElement('li');
+        Li = parent.parentElement;
+        ul_li.removeChild(tick)
+        task_completed_ul.appendChild(Li)
+    })
+
+    let delete_li = document.createElement('img');
+    delete_li.src = "Assets/Images/trash-solid.svg";
+    delete_li.classList.add('delete-icon');
+
+    delete_li.addEventListener('click', (event)=>{
+        localStorage.removeItem(`${li.id}`)
+        let Parent = event.target.parentElement
+        Parent.parentElement.remove()
+    })
+
+    var task_no = li.id
+
+    let ul_li= document.createElement('ul');
+    ul_li.appendChild(tick);
+    ul_li.appendChild(delete_li);
+    li.appendChild(ul_li)
+    ul_li.classList.add('ul-li')
+
+
+    // li.innerText = value;
+    // li.appendChild(tick)
+    // li.appendChild(delete_li);
+    ul.appendChild(li);
+    
+    console.log(value);
+    task_Value.value = "";
+});
+
+delete_li.addEventListener('click', (event)=>{
+    let Parent = event.target.parentElement
+    Parent.remove()
+})
+    
+
+
+task_Value.addEventListener('keypress',(event)=> {
+    if (event.keyCode === 13) {
+        add.click()
+    }
+})
